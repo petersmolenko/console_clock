0314a155d6469b2ad2a1f7d30b1f38400942098a
Peter Smolenko <smolenko.peter@gmail.com>
2020-06-05 22:29:15 +0300

    fix(app.js): changed logic
    changed logic

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 3000;
-const { TIME = 10000, INTERVAL = 1000 } = process.env;
+const { TIME = 5000, INTERVAL = 1000 } = process.env;
+let isActivatedClock = false;
 
 const server = http.createServer((req, res) => {
     if (req.url === "/time") {
-        const printTime = setInterval(() => {
-            console.clear();
-            console.log(new Date(Date.now()).toUTCString());
-        }, INTERVAL);
-        setTimeout(() => {
-            clearInterval(printTime);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/plain");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+
+        if (!isActivatedClock) {
+            const printTime = setInterval(() => {
+                console.clear();
+                console.log(new Date(Date.now()).toUTCString());
+            }, INTERVAL);
+            setTimeout(() => {
+                clearInterval(printTime);
+                res.end(new Date(Date.now()).toUTCString());
+                console.clear();
+                console.log(`Time's up.`);
+                isActivatedClock = true;
+            }, TIME);
+        } else {
             res.end(new Date(Date.now()).toUTCString());
-            console.clear();
-            console.log(`Time's up.`);
-        }, TIME);
+        }
     } else {
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/html; charset=utf-8;");
