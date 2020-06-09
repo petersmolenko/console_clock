e56725145ddc94b2e239baea3852934ca0910461
Peter Smolenko <smolenko.peter@gmail.com>
2020-06-09 07:41:01 +0300

    added client counter

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 const http = require("http");
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 8080;
 const { TIME = 5000, INTERVAL = 1000 } = process.env;
-let isActivatedClock = false;
+let clientCount = 0;
 
 const server = http.createServer((req, res) => {
     if (req.url === "/time") {
+        let printTime = null;
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/plain");
 
-        if (!isActivatedClock) {
-            const printTime = setInterval(() => {
-                console.clear();
+        if (clientCount === 0) {
+            printTime = setInterval(() => {
+                // console.clear();
                 console.log(new Date(Date.now()).toUTCString());
             }, INTERVAL);
-            setTimeout(() => {
-                clearInterval(printTime);
-                res.end(new Date(Date.now()).toUTCString());
-                console.clear();
-                console.log(`Time's up.`);
-                isActivatedClock = true;
-            }, TIME);
-        } else {
-            res.end(new Date(Date.now()).toUTCString());
         }
+
+        clientCount++;
+
+        setTimeout(() => {
+            if (printTime) clearInterval(printTime);
+            res.end(new Date(Date.now()).toUTCString());
+            // console.clear();
+            console.log(`Time's up.`);
+            clientCount--;
+        }, TIME);
     } else {
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/html; charset=utf-8;");
